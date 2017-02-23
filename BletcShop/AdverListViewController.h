bafb55a270e4e8c0eb2bc22a669c0b98f8c92c2f
Bletc <Bletc@robertdeMacBook-Air.local>
2017-02-23 15:57:05 +0800

    添加广告列表

---
+++ BletcShop/AdverListViewController.h
@@ -0,0 +1,13 @@
+//
+//  AdverListViewController.h
+//  BletcShop
+//
+//  Created by Bletc on 2017/2/23.
+//  Copyright © 2017年 bletc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdverListViewController : UIViewController
+
+@end
---
+++ BletcShop/PushAdverViewController.m
@@ -0,0 +1,59 @@
+//
+//  PushAdverViewController.m
+//  BletcShop
+//
+//  Created by Bletc on 2017/2/23.
+//  Copyright © 2017年 bletc. All rights reserved.
+//
+
+#import "PushAdverViewController.h"
+
+#import "AdvertisementHomeVC.h"
+#import "AdverListViewController.h"
+#import "AdverShowViewController.h"
+
+@interface PushAdverViewController ()
+
+@end
+
+@implementation PushAdverViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.navigationItem.title = @"广告投放";
+    // Do any additional setup after loading the view from its nib.
+}
+- (IBAction)pushAdverBtn:(UIButton *)sender {
+    
+    AdvertisementHomeVC *advertisementHomeVC=[[AdvertisementHomeVC alloc]init];
+    [self.navigationController pushViewController:advertisementHomeVC animated:YES];
+    
+}
+- (IBAction)adverListBtn:(id)sender {
+    
+    
+    AdverListViewController *advertisementHomeVC=[[AdverListViewController alloc]init];
+    [self.navigationController pushViewController:advertisementHomeVC animated:YES];
+}
+- (IBAction)adverconfuse:(UIButton *)sender {
+    
+    AdverShowViewController *advertisementHomeVC=[[AdverShowViewController alloc]init];
+    [self.navigationController pushViewController:advertisementHomeVC animated:YES];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
