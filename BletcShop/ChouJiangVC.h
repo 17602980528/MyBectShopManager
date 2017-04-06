268e93264236ea204b53bcba7b242600f1e9e134
Bletc <Bletc@robertdeMacBook-Air.local>
2017-04-06 18:01:05 +0800

    sagsdf

---
+++ BletcShop/ChouJiangVC.h
@@ -0,0 +1,14 @@
+//
+//  ChouJiangVC.h
+//  BletcShop
+//
+//  Created by Bletc on 2017/4/6.
+//  Copyright © 2017年 bletc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChouJiangVC : UIViewController
+@property (nonatomic,copy)NSString *urlString;// <#Description#>
+
+@end
