c65c63294f2d5d68e3f0fefe74be5847d9e8722e
apple <apple@tang.local>
2017-05-25 17:44:23 +0800

    urfvbkdsbv

--- BletcShop/HotNewsVC.m
+++ BletcShop/HotNewsVC.m
@@ -7,12 +7,14 @@
 //
 
 #import "HotNewsVC.h"
-
+#import "WYWebProgressLayer.h"
+#import "UIView+Frame.h"
 @interface HotNewsVC ()<UIWebViewDelegate>
 {
     MBProgressHUD *hud;
     UIWebView *_webView;
     UILabel *label;
+    WYWebProgressLayer *_progressLayer; ///< 网页加载进度条
 }
 @end
 
@@ -24,12 +26,12 @@
     hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
     hud.userInteractionEnabled = YES;
     
-     _webView=[[UIWebView alloc]initWithFrame:CGRectMake(0, 0, SCREENWIDTH, SCREENHEIGHT-64)];
+    _webView=[[UIWebView alloc]initWithFrame:CGRectMake(0, 0, SCREENWIDTH, SCREENHEIGHT-64)];
     _webView.delegate=self;
     [self.view addSubview:_webView];
     [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:self.href]]];
     
-    
+    _progressLayer = [WYWebProgressLayer new];
     if ([self.href containsString:@"http://www.cnconsum.com/cnconsum/helpCenter/"]) {
         _webView.frame = CGRectMake(0, 64, SCREENWIDTH, SCREENHEIGHT-64);
         
@@ -43,15 +45,17 @@
         [self.view addSubview:navView];
         [navView addSubview:btn];
         
-         label=[[UILabel alloc]initWithFrame:CGRectMake(SCREENWIDTH/2-50, 18, 100, 44)];
+        label=[[UILabel alloc]initWithFrame:CGRectMake(SCREENWIDTH/2-50, 18, 100, 44)];
         label.font=[UIFont systemFontOfSize:19.0f];
         label.text=@"常见问题";
         label.textAlignment=1;
         label.textColor=[UIColor whiteColor];
         [navView addSubview:label];
-
-        
-       
+        _progressLayer.frame = CGRectMake(0, 62, SCREEN_WIDTH, 2);
+        [self.view.layer addSublayer:_progressLayer];
+    }else{
+        _progressLayer.frame = CGRectMake(0, 42, SCREEN_WIDTH, 2);
+        [self.navigationController.navigationBar.layer addSublayer:_progressLayer];
     }
     
 }
@@ -60,15 +64,20 @@
     
     [_webView goBack];
     if (!_webView.canGoBack) {
-           [self dismissViewControllerAnimated:YES completion:nil];
-
+        [self dismissViewControllerAnimated:YES completion:nil];
+        
     }
+}
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    [_progressLayer startLoad];
 }
 - (void)webViewDidFinishLoad:(UIWebView *)webView{
     [hud hideAnimated:YES afterDelay:0.0f];
+    [_progressLayer finishedLoad];
 }
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
     [hud hideAnimated:YES afterDelay:5.0];
+    [_progressLayer finishedLoad];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -76,13 +85,13 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
