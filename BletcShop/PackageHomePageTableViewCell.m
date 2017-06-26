6aa45ae19a8439619edabbf7c63af9230a5d8ee2
apple <apple@tang.local>
2017-06-26 15:58:39 +0800

    666

--- BletcShop/PackageHomePageTableViewCell.m
+++ BletcShop/PackageHomePageTableViewCell.m
@@ -12,8 +12,8 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    self.cardImageView.layer.borderWidth=1.0f;
-    self.cardImageView.layer.borderColor=[[UIColor lightGrayColor]CGColor];
+    self.cardImageView.layer.borderWidth=0.6f;
+    self.cardImageView.layer.borderColor=[RGB(238,238,238)CGColor];
     self.cardImageView.layer.cornerRadius=3.0f;
     self.cardImageView.clipsToBounds=YES;
 }
